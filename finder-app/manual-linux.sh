487b6f6f618349c8e048b9f4a531f9fa464317dc
Yarammg <yara.gowayed@etas.com>
2025-02-26 00:15:05 +0100

    Assignment 3-2 yarammg

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -22,7 +22,13 @@ else
 fi
 
 mkdir -p ${OUTDIR}
-
+mkdir -p "${OUTDIR}/arm-cross-compiler"
+cd "${OUTDIR}/arm-cross-compiler"
+wget -O gcc-arm.tar.xz https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+    mkdir -p install 
+    tar x -C install -f gcc-arm.tar.xz 
+    rm -r gcc-arm.tar.xz
+    export PATH="$PATH:/${OUTDIR}/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin"
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
@@ -34,11 +40,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    sudo apt-get update && sudo apt-get install -y --no-install-recommends u-boot-tools kmod cpio flex bison libssl-dev psmisc libncurses-dev
+    sudo apt-get install -y qemu-system-arm
+    make defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    
 fi
 
 echo "Adding the Image in outdir"
-
+echo "The current path is: $(pwd)"
+cp "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ${OUTDIR}
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -47,9 +60,14 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p "rootfs"
+cd "rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
+
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
@@ -60,21 +78,45 @@ else
     cd busybox
 fi
 
-# TODO: Make and install busybox
-
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j$(nproc)
+make CONFIG_PREFIX="${OUTDIR}/rootfs"  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+sudo chmod u+s bin/busybox
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+cd ${OUTDIR}/rootfs
+cp "${OUTDIR}/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1" "lib/"
+cp "${OUTDIR}/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6" "lib64/"
+cp "${OUTDIR}/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2" "lib64/"
+cp "${OUTDIR}/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6" "lib64/"
 
-# TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/tty1 c 4 1
 
-# TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+${CROSS_COMPILE}gcc -o ${OUTDIR}/rootfs/home/writer writer.c
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+#cp $FINDER_APP_DIR/writer ${OUTDIR}/rootfs/home
+cp $FINDER_APP_DIR/finder.sh ${OUTDIR}/rootfs/home
+cp $FINDER_APP_DIR/finder-test.sh ${OUTDIR}/rootfs/home
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp $FINDER_APP_DIR/conf/username.txt ${OUTDIR}/rootfs/home/conf/username.txt
+cp $FINDER_APP_DIR/conf/assignment.txt  ${OUTDIR}/rootfs/home/conf/assignment.txt
+cp $FINDER_APP_DIR/autorun-qemu.sh  ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -o --owner root:root > ${OUTDIR}/rootfs/initramfs.cpio
+gzip -f initramfs.cpio
+cp "initramfs.cpio.gz" ${OUTDIR}
+
+
