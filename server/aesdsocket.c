51c2a8a467c31bba35e76605cc7f23f0da60110b
Yarammg <yara.gowayed@etas.com>
2025-03-08 14:24:26 +0100

    fix assign 5-1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,144 +7,170 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <signal.h>
-#include <unistd.h> 
+#include <unistd.h>
+
 #define BUFFER_SIZE 1024
+
 const char *filename = "/var/tmp/aesdsocketdata";
-int keep_running = 1;
+volatile sig_atomic_t keep_running = 1;
+int clientfd, sockfd;
+
 void aesdsocket_signal_handler(int signo) {
     if (signo == SIGINT || signo == SIGTERM) {
-        printf("Caught signal, exiting");
+        printf("Caught signal, exiting\n");
+        if (clientfd > 0)
+    	{
+            close(clientfd);
+    	}
+    	if (sockfd > 0)
+    	{
+            close(sockfd);
+    	}
         keep_running = 0;
     }
 }
-int main(int argc, char *argv[])
-{
-printf("1");
-  int daemon_mode = 0;
-  if (argc > 1 && strcmp(argv[1], "-d") == 0)
-  {
-      daemon_mode = 1;
-  }
-  printf("1");
-  int status, new_fd;
-  struct sockaddr_storage their_addr;
-  socklen_t addr_size;
-  struct addrinfo hints;
-  struct addrinfo *servinfo;  // will point to the results
-  char client_ip[INET_ADDRSTRLEN]; 
-  memset(&hints, 0, sizeof hints); // make sure the struct is empty
-  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-  if ((status = getaddrinfo(NULL, "9000", &hints, &servinfo)) != 0) 
-  {
-      fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-      exit(1);
-  }
-  
-  int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-  if (sockfd < 0) 
-  {
-    printf("Error opening socket: %s\n", strerror(errno));
-    exit(1);
-  }
-  
-  int yes=1;
-  
-  // lose the pesky "Address already in use" error message
-  if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) 
-  {
-      perror("setsockopt");
-      exit(1);
-  } 
-  
-  int res = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
-  if (res < 0) 
-  {
-    printf("Error binding to socket: %s\n", strerror(errno));
-    exit(1);
-  }
-  
-  // Start listening for connections
-  if (listen(sockfd, 10) == -1) 
-  {
+int main(int argc, char *argv[]) {
+    signal(SIGINT, aesdsocket_signal_handler);
+    signal(SIGTERM, aesdsocket_signal_handler);
+
+    int daemon_mode = 0;
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        daemon_mode = 1;
+    }
+
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int status;
+    if ((status = getaddrinfo(NULL, "9000", &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sockfd < 0) {
+        perror("Error opening socket");
+        freeaddrinfo(servinfo);
+        exit(1);
+    }
+
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("setsockopt");
+        freeaddrinfo(servinfo);
+        exit(1);
+    }
+
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+        perror("Error binding to socket");
+        freeaddrinfo(servinfo);
+        exit(1);
+    }
+
+    if (listen(sockfd, 10) == -1) {
         perror("listen failed");
-        close(sockfd);
         exit(1);
-  }
-    // Fork if daemon mode is enabled
-    if (daemon_mode)
-    {
-      pid_t pid = fork();
-      if (pid < 0) 
-      {
-        perror("fork failed");
-        close(sockfd);
-        exit(1);
-      }
-      if (pid > 0) 
-      {
-        // Parent process
-        exit(0);
-      }
-      // Child process becomes daemon
+    }
+
+    if (daemon_mode) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        }
+        if (pid > 0) {
+            exit(0);
+        }
 
         setsid();
         chdir("/");
-        // Open /dev/null
+        
+        // Close standard file descriptors
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
         int fd = open("/dev/null", O_RDWR);
-        if (fd == -1)
-        {
-          perror("open devnull failed");
-          return -1;
+        if (fd != -1) {
+            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            close(fd);
         }
-        // Redirect standard file descriptors to /dev/null
-        dup2(fd, STDIN_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        dup2(fd, STDERR_FILENO);
-    }
-  addr_size = sizeof their_addr;
-  while(keep_running)
-  {
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-    if(new_fd < 0)
-    {
-      printf("Error accepting the connection: %s\n", strerror(errno));
-      exit(1);
     }
 
-    if (getnameinfo((struct sockaddr*)&their_addr, addr_size, client_ip, sizeof(client_ip), NULL, 0, NI_NUMERICHOST) == 0) 
-    {
-      //printf("Accepted connection from %s\n", client_ip);
-    } else {
-      perror("getnameinfo failed");
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof their_addr;
+    char client_ip[INET_ADDRSTRLEN];
+
+    while (keep_running) {
+        clientfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (clientfd < 0) {
+            perror("Error accepting connection");
+            continue;
+        }
+
+        if (getnameinfo((struct sockaddr*)&their_addr, addr_size, client_ip, sizeof(client_ip), NULL, 0, NI_NUMERICHOST) == 0) {
+            printf("Accepted connection from %s\n", client_ip);
+        } else {
+            perror("getnameinfo failed");
+        }
+
+        char buffer[BUFFER_SIZE];
+        ssize_t bytes_read;
+        while ((bytes_read = read(clientfd, buffer, sizeof(buffer))) > 0) 
+        {
+          char *newline = memchr(buffer, '\n', bytes_read);
+            if (newline)
+            {
+                size_t line_length = newline - buffer + 1;
+                int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                if (fd == -1) {
+                    perror("Error opening file");
+                    continue;
+                }
+                write(fd, buffer, line_length);
+                close(fd);
+                
+                int fd_read = open(filename, O_RDONLY);
+                if (fd_read == -1) {
+                    perror("Error opening file");
+                    continue;
+                }
+                char buffer2[BUFFER_SIZE];
+                while ((bytes_read = read(fd_read, buffer2, sizeof(buffer2))) > 0) 
+                {
+                    send(clientfd, buffer2, bytes_read, 0);
+                }
+                if (bytes_read ==-1)
+                {
+                printf("Error reading from file");
+                }
+                close(fd_read);
+            }
+            else
+            {
+                int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                if (fd == -1) {
+                    perror("Error opening file");
+                    continue;
+                }
+                write(fd, buffer, bytes_read);
+                close(fd);
+            }
+        }
+        shutdown(clientfd, SHUT_RDWR);
+        close(clientfd);
     }
-    
-    int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);  // Open for writing, create if not exists
-    if (fd == -1) 
-    {
-      perror("Error opening file");
-      return 1;
-    }
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
-    while ((bytes_read = read(new_fd, buffer, sizeof(buffer))) > 0)
-    {
-      char *newline = memchr(buffer, '\n', bytes_read);
-      size_t line_length = newline - buffer + 1;
-      write(fd, newline, line_length);
-      while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0) 
-      {
-       send(new_fd, buffer, bytes_read, 0);
-      }
-    }  
-    close(fd);
-    printf("Closed connection from %s\n", client_ip);
-    shutdown(new_fd, 2);
-  }
-  remove(filename);
-  shutdown(sockfd,2);
-  freeaddrinfo(servinfo);
-  return 0;
+
+    remove(filename);
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
+    freeaddrinfo(servinfo);
+    return 0;
 }
+
